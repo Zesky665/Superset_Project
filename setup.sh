1b72aaba37f97ec751ea92e8f49e707b5502a5cd
Zesky665 <zharkoc@gmail.com>
2024-11-28 17:46:27 +0100

    Finish setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
-docker exec -it superset superset fab create-admin \
+# Wait for Superset to be ready
+sleep 10
+
+docker exec superset superset fab create-admin \
               --username admin \
               --firstname Superset \
               --lastname Admin \
               --email admin@localhost.com \
               --password secret
 
-docker exec -it superset superset db upgrade &&
-         docker exec -it superset superset init
+docker exec superset superset db upgrade &&
+         docker exec superset superset init
+
+echo "Done"
